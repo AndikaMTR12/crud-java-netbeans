c95631c7474d9a03a50b8fb778e1dbd1da84ffc6
AndikaMTR12 <andiseven06@google.com>
2023-06-15 16:25:27 +0800

    upload

---
+++ src/MenuUtama.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import form.frmBarang;
+/**
+ *
+ * @author acer
+ */
+public class MenuUtama {
+    public static void main(String[] args){
+        frmBarang barang = new frmBarang();
+        barang.setVisible(true);
+    }
+}
